606a19af21e29221a75be114fd92a2852cfd62df
Jenn Briden <jennbriden@gmail.com>
2018-06-27 09:32:08 -0700

    Added starter project for template demo

---
+++ src/main/java/com/jBriden/jbridenwebappusingtemplate/JbridenWebappUsingTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.jBriden.jbridenwebappusingtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbridenWebappUsingTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbridenWebappUsingTemplateApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jBriden/jbridenwebappusingtemplate/JbridenWebappUsingTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jBriden.jbridenwebappusingtemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JbridenWebappUsingTemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
